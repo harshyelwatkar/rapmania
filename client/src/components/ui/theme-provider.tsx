931d9ba55311c15b5982c83b8c87efc88c76f96d
harshyelwatkar <yelwatkarharsh@gmail.com>
2025-04-30 16:55:51 +0530

    Added theme-provide file

--- client/src/components/ui/theme-provider.tsx
+++ client/src/components/ui/theme-provider.tsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
-
-export function ThemeProviderWrapper({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
-=======
-// client/src/components/ui/theme-provider.tsx
 import { ReactNode } from "react";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
->>>>>>> 9b1fe2f (Finalize frontend logic, cleanup, and prepare for Vercel deployment)
